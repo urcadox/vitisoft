1729b66c7a4f940e2f943e86a29ca92bfa7f553c
Alexandre Berthaud <alexandre@berthaud.me>
2016-08-23 11:57:52 +0200

    Add Plot MongoDB DAO

--- app/models/entities/Plot.scala
+++ app/models/entities/Plot.scala
@@ -1,8 +1,16 @@
 package models.entities
 
+import javax.inject._
+import com.google.inject.ImplementedBy
 import java.util.UUID
 import java.time.OffsetDateTime
 import play.api.libs.json._
+import play.modules.reactivemongo._
+import reactivemongo.api._
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 
 case class Plot (
   id: Plot.PlotID,
@@ -14,4 +22,26 @@ case class Plot (
 
 object Plot {
   type PlotID = UUID
+  implicit val plotFormat: OFormat[Plot] = Json.format[Plot]
+}
+
+@ImplementedBy(classOf[PlotDAOMongoDB])
+trait PlotDAO {
+  def getAll(): Future[List[Plot]]
+  def getById(id: UUID): Future[Option[Plot]]
+}
+
+class PlotDAOMongoDB @Inject() (
+  implicit val mongo: ReactiveMongoApi,
+  val ec: ExecutionContext
+) extends PlotDAO {
+  val collection = mongo.db.collection[JSONCollection]("plots")
+
+  def getAll(): Future[List[Plot]] = {
+    collection.find(Json.obj()).cursor[Plot]().collect[List]()
+  }
+
+  def getById(id: UUID): Future[Option[Plot]] = {
+    collection.find(Json.obj("id" -> id)).cursor[Plot]().headOption
+  }
 }
