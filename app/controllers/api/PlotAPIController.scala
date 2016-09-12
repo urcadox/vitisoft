b749195743848b4b0a451f188dfc3cd6bf495484
Alexandre Berthaud <alexandre@berthaud.me>
2016-09-12 16:16:59 +0200

    Add basic API endpoints

---
+++ app/controllers/api/PlotAPIController.scala
@@ -0,0 +1,37 @@
+package controllers.api
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import scala.concurrent.{ExecutionContext, Future}
+import models.entities.{Plot, PlotDAO, PlotInput}
+
+@Singleton
+class PlotAPIController @Inject() (
+  val plotDAO: PlotDAO,
+  implicit val ec: ExecutionContext
+) extends Controller {
+
+  def plots = Action.async { implicit request =>
+    plotDAO.getAll map { plots =>
+      Ok(Json.toJson(plots))
+    }
+  }
+
+  def plot(id: Plot.PlotID) = Action.async { implicit request =>
+    plotDAO.getById(id) map { plotOption =>
+      plotOption map { plot =>
+        Ok(Json.toJson(plot))
+      } getOrElse NotFound
+    }
+  }
+
+  def editPlot(id: Plot.PlotID) = Action.async(parse.json) { implicit request =>
+    request.body.validate[PlotInput].fold(
+      e => Future.successful(BadRequest(JsError.toJson(e))),
+      plot => plotDAO.update(id, plot).map(plot => Ok(Json.toJson(plot)))
+    )
+  }
+
+}
